d79975f365d78f966cac4a6a0abe095fdc80eece
kzt07 <kotharikshitiz2@gmail.com>
2025-06-10 13:46:31 +0530

    Update BiotechStudent.java

--- Bsrc/BiotechStudent.java
+++ Bsrc/BiotechStudent.java
@@ -1,1 +1,42 @@
+import java.io.Serializable;
+import java.util.Map;
 
+class Student implements Serializable {
+   private String name;
+   private String rollNo;
+   private Map<String, String> subjectGrades;
+
+   public Student(String var1, String var2, Map<String, String> var3) {
+      this.name = var1;
+      this.rollNo = var2;
+      this.subjectGrades = var3;
+   }
+
+   public String getName() {
+      return this.name;
+   }
+
+   public String getRollNo() {
+      return this.rollNo;
+   }
+
+   public Map<String, String> getSubjectGrades() {
+      return this.subjectGrades;
+   }
+
+   public void setName(String var1) {
+      this.name = var1;
+   }
+
+   public void setRollNo(String var1) {
+      this.rollNo = var1;
+   }
+
+   public void setSubjectGrades(Map<String, String> var1) {
+      this.subjectGrades = var1;
+   }
+
+   public String toString() {
+      return this.name + " (" + this.rollNo + ")";
+   }
+}
